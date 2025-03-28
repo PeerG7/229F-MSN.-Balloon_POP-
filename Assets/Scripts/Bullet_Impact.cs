ff9fdc4c78d22af2f4687b88220c242763f45315
sho-ikeda1 <sho.iked@bumail.net>
2025-03-29 06:40:06 +0700

    ใส่ script bullet impact ไป แต่ยังไม่ได้ใช้ เอามาเผื่อ

---
+++ Assets/Scripts/Bullet_Impact.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Bullet_Impact : MonoBehaviour
+{
+    public AudioClip impactSound; // เสียงกระทบ
+    private AudioSource audioSource;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>(); // เพิ่ม AudioSource อัตโนมัติ
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (impactSound != null && audioSource != null)
+        {
+            audioSource.PlayOneShot(impactSound); // เล่นเสียงเมื่อกระสุนชน
+        }
+
+        Destroy(gameObject, 0.5f); // ทำลายกระสุนหลังจาก 0.5 วินาที
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
