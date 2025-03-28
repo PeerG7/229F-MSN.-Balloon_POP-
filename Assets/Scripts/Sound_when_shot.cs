2c03a1315eb2fa2f9665fdd979ed83df103b2275
sho-ikeda1 <sho.iked@bumail.net>
2025-03-29 06:14:40 +0700

    sound when shot เสร็จ

--- Assets/Scripts/Sound_when_shot.cs
+++ Assets/Scripts/Sound_when_shot.cs
@@ -1,16 +1,29 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Audio;
 
 public class Sound_when_shot : MonoBehaviour
 {
+    public AudioSource Source;  // ตัวเล่นเสียง
+    public AudioClip Sound;  // ไฟล์เสียงปุ่ม
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
+        if (Source == null)
+            Source = GetComponent<AudioSource>(); // ค้นหา AudioSource ใน GameObject นี้
 
+        if (Sound != null)
+            Source.clip = Sound; // ตั้งค่าเสียง
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W)) // ตรวจจับการกดปุ่ม W
+        {
+            if (Source != null && Sound != null)
+            {
+                Source.PlayOneShot(Sound); // เล่นเสียงเมื่อกด W
+            }
+        }
     }
 }
